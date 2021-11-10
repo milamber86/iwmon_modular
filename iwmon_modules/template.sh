f1b503b93c21b6217700fd8c9d711ea25cc62bf6
milamber86 <beranek@icewarp.cz>
2021-11-10 17:01:22 +0100

    Create template.sh

---
+++ iwmon_modules/template.sh
@@ -0,0 +1,39 @@
+cleanup() {
+/usr/bin/rm -f "${outputpath}/${myname}.lck"
+}
+trap cleanup INT TERM ERR EXIT
+
+# read setting from configfile
+readcfg() { # ( setting_name -> setting_value )
+result="$(/usr/bin/grep "${1}" ${configdir}/iwmon.cfg | awk -F ':' '{print $2}' )";
+if [ -z "${result}" ]
+  then
+  echo "Variable ${1} empty or not found";
+  return 1
+  else
+  echo "${result}"
+  return 0
+fi
+}
+
+# write log to local syslog
+function slog
+{
+local logsvr="${1}";
+local logmsg="${2}";
+local logdate="$(date '+%b %d %H:%M:%S')";
+timeout -k 2 2 /usr/bin/logger "${logdate} ${HOSTNAME} IWMON: ${logsvr} ${logmsg}"
+}
+
+myname="$(basename "$0" | awk -F'.' '{print $1}')";
+configdir="/opt/icewarp/scripts/";
+outputpath="$(readcfg outputpath)";
+outputfile="${outputpath}/${myname}.mon";
+nfstestfile="/mnt/data/storage.dat";
+toolSh="/opt/icewarp/tool.sh";
+
+# MAIN
+touch ${outputpath}/${myname}.lck
+# < place your check here >
+# < and return the result to >
+# < ${outputpath}/${myname}.mon >
